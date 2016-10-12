6716657ed490bba9eec37a1764ecbd032c3f0231
Sandra Killebrew <sandra@nextconsulting-llc.com>
2016-10-11 20:35:12 -0500

    changed port to 3030

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+//require the express module
+const express = require('express');
 
+//create a new instance modeule
+const app = express();
+
+//declare our route
+app.get('/:message', function(request, response, next){
+  const message = request.params.message;
+  console.log(request.params);
+  return response.json(message);
+});
+
+//set our port to serve the application on
+const port = 3030;
+
+//tell our instance of express to listen to request made on our port
+app.listen(port, function() {
+  console.log(`Listening on port: ${port}`);
+})
